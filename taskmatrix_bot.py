1d7ac55d53f4a4516fb07cef0b68f45cd0230673
kaoekb <kaoekb@gmail.com>
2024-01-26 10:09:07 +0500

    1

--- taskmatrix_bot.py
+++ taskmatrix_bot.py
@@ -1,35 +1,22 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
-import asyncio
+# from aiogram.utils import executor
+from aiogram import executor
+from aiogram.types.web_app_info import WebAppInfo
 import os
 from dotenv import load_dotenv, find_dotenv
-from aiogram.filters import CommandStart
 
-# Загрузка переменных окружения
 load_dotenv(find_dotenv())
 
-# Получение токена бота из переменных окружения
 TELEGRAM_TOKEN = os.getenv("Token_tg")
 
-async def start(message: Message):
+# @taskmatrix_bot.:
+bot = Bot(TELEGRAM_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.KeyboardButton(text="Открыть матрицу задач", web_app=types.WebAppInfo(url="https://taskmatrix.online/")))
-    await message.answer("Матрица задач", reply_markup=markup)
+    markup.add(types.KeyboardButton(text="Открыть квадрат", web_app=WebAppInfo(url="https://taskmatrix.online/")))
+    await message.answer("Квадрат", reply_markup=markup)
 
-async def main():
-    # Инициализация бота
-    bot = Bot(TELEGRAM_TOKEN)
-
-    # Инициализация диспетчера с привязкой бота
-    dp = Dispatcher()
-    dp.set_bot(bot)
-
-    # Регистрация обработчика команды /start
-    dp.message.register(start, CommandStart())
-
-    # Запуск поллинга с передачей экземпляра бота
-    await dp.start_polling(bot)
-
-# Запуск асинхронного цикла
-if __name__ == '__main__':
-    asyncio.run(main())
+executor.start_polling(dp)
